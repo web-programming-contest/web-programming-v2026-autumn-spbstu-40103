ea7659b25612ef39b270a34b4b5b76c95882d9c6
edwin <edwindenisovelers9864@gmail.com>
2026-09-10 14:23:48 +0300

    fix ci 2

--- denisov-elers.edwin/lab5/src/main.tsx
+++ denisov-elers.edwin/lab5/src/main.tsx
@@ -1,6 +1,5 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import './styles.css';
 import App from './App';
 
 const rootElement = document.querySelector('[data-testid="app"]');
