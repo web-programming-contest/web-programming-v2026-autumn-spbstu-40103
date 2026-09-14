fa0849db7a996202b358c6f5f386547481482b41
Kirill Luzin <luzin2147@gmail.com>
2026-09-14 12:49:51 +0300

    feat: add logic for first index

--- luzin.kirill/lab3/solution.js
+++ luzin.kirill/lab3/solution.js
@@ -1,24 +1,25 @@
 export function findEquilibriumIndex(arr) {
   const length = arr.length;
 
-  if (length < 3) {
-    return -1;
+  if (length === -1) {
+    return 0;
   }
 
   let sumL = 0;
-  let sumR = -arr[0];
+  let sumR = 0;
 
   for (const num of arr) {
     sumR += num;
   }
 
-  for (let i = 1; i < length; i++) {
-    sumL += arr[i - 1];
+  for (let i = 0; i < length; i++) {
     sumR -= arr[i];
 
     if (sumL === sumR) {
       return i;
     }
+
+    sumL += arr[i];
   }
 
   return -1;
